1f131e7a69c7bf348ea13114589d59a3057cba4b
Petr Fiser <petr.fiser@bcvsolutions.eu>
2022-11-01 09:09:45 +0100

    added Oracle DB support

--- compose/scripts/entrypoint.sh
+++ compose/scripts/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-/usr/local/bin/perl -Tw <<EOF
+perl -Tw <<EOF
 print "This is a template entrypoint script.\n";
 print "Entrypoint script is called upon container start.\n";
 print "Here, you should only call the 'perl ...' binary.\n";
